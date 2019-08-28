5705e5088e43764a372ccdc79d855f42a466e169
schoener-leben <50426853+schoener-leben@users.noreply.github.com>
2019-08-28 14:45:40 +0200

    Revert "4.2"

--- resources/js/src/app/exceptions/ExceptionMap.js
+++ resources/js/src/app/exceptions/ExceptionMap.js
@@ -12,7 +12,6 @@ export const exceptionMap = new Map(
         ["9", "notificationsBasketItemsRemoved"],
         ["10", "notificationsBasketItemsRemovedForLanguage"],
         ["11", "notificationsNoEmailEntered"],
-        ["12", "notificationsWarningOverselling"],
         ["110", "errorBasketItemVariationNotFound"],
         ["111", "errorBasketItemNotEnoughStockForVariation"],
         ["112", "errorBasketItemMaximumQuantityReachedForItem"],
